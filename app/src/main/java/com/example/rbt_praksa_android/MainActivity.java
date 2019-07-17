f3bf487da5f5109f6cd2cd8feaff2ad70e63fd8a
Nemanja Savkic <nemanja.savkic92@gmail.com>
2019-07-17 17:10:35 +0200

    - Day 2 - Added second activity with funcionality (Change image and switch on button click and change text on edit)

--- app/src/main/java/com/example/rbt_praksa_android/MainActivity.java
+++ app/src/main/java/com/example/rbt_praksa_android/MainActivity.java
@@ -7,24 +7,26 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
-//    private Button secondActivityButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        secondActivityButton = findViewById(R.id.second_activity_button);
 
-//        secondActivityButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent nextActivity = new Intent();
-//                startActivity(nextActivity);
-//            }
-//        });
+        Button welcome_button = findViewById(R.id.welcome_button);
+
+        welcome_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                welcome();
+            }
+        });
+
     }
 
     @Override
@@ -48,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d("lifecycle", "onSop()");
+        Log.d("lifecycle", "onStop()");
     }
 
     @Override
@@ -57,8 +59,8 @@ public class MainActivity extends AppCompatActivity {
         Log.d("lifecycle", "onDestroy()");
     }
 
-    protected void welcome(View view) {
-        Intent welcome = new Intent(this, SecondActivity.class);
-        startActivity(welcome);
+    protected void welcome() {
+        Intent homeActivity = new Intent(this, HomeActivity.class);
+        startActivity(homeActivity);
     }
 }
